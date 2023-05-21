57237582b5de5225c3332085b3c10b999df6fb99
Uriel thoma <108682061+UrielCT@users.noreply.github.com>
2023-05-21 19:15:36 -0300

    Create home

--- js/main.js
+++ js/main.js
@@ -3,21 +3,28 @@ function cargarDatos(){
     fetch('./src/lamparas.json')
     .then(response => response.json())
     .then(data => {
-        const contenedor = document.getElementById('contenedor');
+        const contenedor = document.getElementById('contenedor_lamparas');
         let div = ``;
 
         data.lamparas.forEach(lampara => {
             div += `
-            <div class="card">
-                <img src="${lampara.imagen}" alt="${lampara.nombre}">
-                <h2>${lampara.nombre} </h2>
-                <p>Edad: ${lampara.descripcion}</p>
-                <h3>${lampara.precio} </h2>
+            <div class="lampara">
+                <img class="lampara__imagen" src="${lampara.imagen}" alt="${lampara.nombre}">
+                <div class="lampara__contenido">
+                    <h2 class="lampara__nombre">${lampara.nombre} </h2>
+                    <p class="lampara__descripcion">${lampara.descripcion}</p>
+                    <h3 class="lampara__precio">$${lampara.precio} </h2>
+                </div>
             </div>
             `;
-            
-            contenedor.innerHTML = div;
         });
+
+        div += `
+            <div class="grafico grafico--imagenA"></div>
+            <div class="grafico grafico--imagenB"></div>
+        `;
+
+        contenedor.innerHTML = div;
     });
 }
 cargarDatos();
